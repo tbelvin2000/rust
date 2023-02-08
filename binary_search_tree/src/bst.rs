2f5518941f5baed4825498ff87d64ea95c6423b9
tbelvin2000 <tombelvin@gmail.com>
2023-02-08 13:42:02 -0500

    Messed with bst

--- binary_search_tree/src/bst.rs
+++ binary_search_tree/src/bst.rs
@@ -1,5 +1,26 @@
-struct Node {
+struct Node<'a> {
     key: u32,
-    left_child: &Node,
-    right_child: &Node,
+    left_child: Option<&'a Node<'a>>,
+    right_child: Option<&'a Node<'a>>,
 }
+// Lifetimes are saying that struct Node will have Option pointers to nodes with at maximum
+//     the same lifetime as itself. Is there a way to specify they will have a lesser lifespan?
+
+impl<'a> Node<'a> {
+    // Search tree rooted at self for target node
+    // Return reference to Node or none
+    fn find(&self, target: u32) -> Option<&Node> {
+        todo!("find method")
+    }
+    // Insert node with key in tree rooted at self
+    // Return true if inserted, false if key is already in tree
+    fn insert(&mut self, key: u32) -> bool{
+        todo!("Insert method")
+    }
+    // Delete target node from tree rooted at self
+    // Return true if deleted, false if key is not in tree
+    fn delete(&mut self, target: u32) -> bool{
+        todo!("Delete method")
+    }
+}
+
--- binary_search_tree/target/debug/deps/binary_search_tree-4cc8efb1dff62317.d
+++ binary_search_tree/target/debug/deps/binary_search_tree-4cc8efb1dff62317.d
@@ -1,5 +1,6 @@
-/home/thomas/rust/binary_search_tree/target/debug/deps/binary_search_tree-4cc8efb1dff62317: src/main.rs
+/home/thomas/rust/binary_search_tree/target/debug/deps/binary_search_tree-4cc8efb1dff62317: src/main.rs src/bst.rs
 
-/home/thomas/rust/binary_search_tree/target/debug/deps/binary_search_tree-4cc8efb1dff62317.d: src/main.rs
+/home/thomas/rust/binary_search_tree/target/debug/deps/binary_search_tree-4cc8efb1dff62317.d: src/main.rs src/bst.rs
 
 src/main.rs:
+src/bst.rs:
