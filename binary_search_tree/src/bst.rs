c00ab589359a6a2d2d8b7bd6529ecf99608d0bfe
tbelvin2000 <tombelvin@gmail.com>
2023-02-10 16:18:15 -0500

    yeet

--- binary_search_tree/src/bst.rs
+++ binary_search_tree/src/bst.rs
@@ -9,7 +9,11 @@ struct Node {
 impl Default for Node {
     // Should only be used to with a passed key value
     fn default() -> Self {
-        Node { key: 0, left_child: None, right_child: None }
+        Node {
+            key: 0,
+            left_child: None,
+            right_child: None,
+        }
     }
 }
 
@@ -26,8 +30,8 @@ impl Node {
             x if x > self.key => match &self.right_child {
                 None => None,
                 Some(rc) => rc.find(target),
-            }
-            _ => panic!("x is somehow not related to self.key")
+            },
+            _ => panic!("x is somehow not related to self.key"),
         }
     }
     // Insert node with key in tree rooted at self
@@ -36,31 +40,37 @@ impl Node {
         match key {
             // Key already exists in tree
             x if x == self.key => false,
-            x if x < self.key => match &mut self.left_child{
+            x if x < self.key => match &mut self.left_child {
                 // Key is left child of current node
                 None => {
-                    self.left_child = Some(Box::new(Node{key, ..Default::default()}));
+                    self.left_child = Some(Box::new(Node {
+                        key,
+                        ..Default::default()
+                    }));
                     true
-                },
+                }
                 // Key is left descendent of current node
                 Some(lc) => lc.insert(key),
             },
-            x if x > self.key => match &mut self.right_child{
+            x if x > self.key => match &mut self.right_child {
                 // Key is right child of current node
                 None => {
-                    self.right_child = Some(Box::new(Node{key, ..Default::default()}));
+                    self.right_child = Some(Box::new(Node {
+                        key,
+                        ..Default::default()
+                    }));
                     true
-                },
+                }
                 // Key is right descendent of current node
                 Some(rc) => rc.insert(key),
             },
-            _ => panic!("x is sommehow not related to self.key")
+            _ => panic!("x is sommehow not related to self.key"),
         }
     }
     // Delete target node from tree rooted at self
     // Return true if deleted, false if key is not in tree
     fn delete(&mut self, target: u32) -> Node {
-        fn successor(target:&mut Node) -> Option<Box<Node>> {
+        fn successor(target: &mut Node) -> Option<Box<Node>> {
             match (target.left_child, target.right_child) {
                 // Target is leaf
                 (None, None) => None,
@@ -69,28 +79,45 @@ impl Node {
                 // Target has only right child
                 (None, rc) => rc,
                 // Target has both children; Find in order successor
+                // Just use parent and look at child, that way there is only one pointer
                 (lc, Some(rc)) => {
-                    let mut parent = target;
-                    let mut succ = rc.as_mut();
-                    loop {
-                        match succ.left_child {
-                            None => break,
-                            Some(rc) => {
-                                parent = succ;
-                                succ = rc.as_mut();
+                    let mut parent = rc.as_mut();
+                    match parent.left_child {
+                        // Successor is targets right child
+                        None => {
+                            let succ = Some(Box::new(Node{key: parent.key, left_child: target.left_child, right_child: parent.right_child}));
+                            parent.delete(parent.key);
+                            return succ;
+                        }
+                        // Successor is leftmost descendent
+                        Some(lc) => {
+                            /* Find leftmost descendent then build and return successor */
+                            loop {
+                                parent = lc.as_mut();
+                                match parent.left_child {
+                                    
+                                }
                             }
                         }
                     }
                     let key = succ.key;
                     let left_child = target.left_child;
-                    let right_child = if target.right_child.unwrap().as_ref().key != key {target.right_child } else {succ.right_child};
+                    let right_child = if target.right_child.unwrap().as_ref().key != key {
+                        target.right_child
+                    } else {
+                        succ.right_child
+                    };
                     parent.delete(key);
-                    Some(Box::new(Node{key, left_child, right_child}))
+                    Some(Box::new(Node {
+                        key,
+                        left_child,
+                        right_child,
+                    }))
                 }
             }
         }
         match target {
-            x if x == self.key => successor(self)
+            x if x == self.key => successor(self),
         }
     }
 }
