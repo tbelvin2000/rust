d99b9c14797f7ecd91365169a84a78882a1f5e7e
tbelvin2000 <tombelvin@gmail.com>
2023-02-10 15:12:18 -0500

    SHits wack yo

--- binary_search_tree/src/bst.rs
+++ binary_search_tree/src/bst.rs
@@ -1,22 +1,29 @@
-struct Node<'a> {
+struct Node {
     key: u32,
-    left_child: Option<&'a Node<'a>>,
-    right_child: Option<&'a Node<'a>>,
+    left_child: Option<Box<Node>>,
+    right_child: Option<Box<Node>>,
 }
 // Lifetimes are saying that struct Node will have Option pointers to nodes with at maximum
 //     the same lifetime as itself. Is there a way to specify they will have a lesser lifespan?
 
-impl<'a> Node<'a> {
+impl Default for Node {
+    // Should only be used to with a passed key value
+    fn default() -> Self {
+        Node { key: 0, left_child: None, right_child: None }
+    }
+}
+
+impl Node {
     // Search tree rooted at self for target node
     // Return reference to Node or none
     fn find(&self, target: u32) -> Option<&Node> {
         match target {
-            x if x == self.key => Some(&self),
-            x if x < self.key => match self.left_child {
+            x if x == self.key => Some(self),
+            x if x < self.key => match &self.left_child {
                 None => None,
                 Some(lc) => lc.find(target),
             },
-            x if x > self.key => match self.right_child {
+            x if x > self.key => match &self.right_child {
                 None => None,
                 Some(rc) => rc.find(target),
             }
@@ -27,20 +34,63 @@ impl<'a> Node<'a> {
     // Return inserted Node if inserted, None if key is already in tree
     fn insert(&mut self, key: u32) -> bool {
         match key {
+            // Key already exists in tree
             x if x == self.key => false,
-            x if x < self.key => match self.left_child{
+            x if x < self.key => match &mut self.left_child{
+                // Key is left child of current node
                 None => {
-                    let n:Node<'a> = Node{key, left_child:None, right_child:None};
-                    self.left_child = Some(&n);
-                    return true
-                }
-                Some(lc) => lc.insert(key)
-            }
+                    self.left_child = Some(Box::new(Node{key, ..Default::default()}));
+                    true
+                },
+                // Key is left descendent of current node
+                Some(lc) => lc.insert(key),
+            },
+            x if x > self.key => match &mut self.right_child{
+                // Key is right child of current node
+                None => {
+                    self.right_child = Some(Box::new(Node{key, ..Default::default()}));
+                    true
+                },
+                // Key is right descendent of current node
+                Some(rc) => rc.insert(key),
+            },
+            _ => panic!("x is sommehow not related to self.key")
         }
     }
     // Delete target node from tree rooted at self
     // Return true if deleted, false if key is not in tree
-    fn delete(&mut self, target: u32) -> bool {
-        todo!("Delete method")
+    fn delete(&mut self, target: u32) -> Node {
+        fn successor(target:&mut Node) -> Option<Box<Node>> {
+            match (target.left_child, target.right_child) {
+                // Target is leaf
+                (None, None) => None,
+                // Target has only left child
+                (lc, None) => lc,
+                // Target has only right child
+                (None, rc) => rc,
+                // Target has both children; Find in order successor
+                (lc, Some(rc)) => {
+                    let mut parent = target;
+                    let mut succ = rc.as_mut();
+                    loop {
+                        match succ.left_child {
+                            None => break,
+                            Some(rc) => {
+                                parent = succ;
+                                succ = rc.as_mut();
+                            }
+                        }
+                    }
+                    let key = succ.key;
+                    let left_child = target.left_child;
+                    let right_child = if target.right_child.unwrap().as_ref().key != key {target.right_child } else {succ.right_child};
+                    parent.delete(key);
+                    Some(Box::new(Node{key, left_child, right_child}))
+                }
+            }
+        }
+        match target {
+            x if x == self.key => successor(self)
+        }
     }
 }
---
+++ linked_list/target/debug/deps/linked_list-1e562a88673472ff.d
@@ -0,0 +1,9 @@
+/home/thomas/rust/linked_list/target/debug/deps/linked_list-1e562a88673472ff.rmeta: src/main.rs src/linked_list.rs Cargo.toml
+
+/home/thomas/rust/linked_list/target/debug/deps/linked_list-1e562a88673472ff.d: src/main.rs src/linked_list.rs Cargo.toml
+
+src/main.rs:
+src/linked_list.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
